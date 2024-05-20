698905c81de9f7c3132c3c60546989e51afd93c6
GraficaMM <grafica.multimedia.2023@gmail.com>
2024-05-20 21:39:37 +0200

    logo

---
+++ components/ProductDetail.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <div class="grid grid-cols-2">
+        <img :src="product.image" alt="">
+    </div>
+    <div>
+        <h2>{{ product.title }}</h2>
+        <p>{{ product.price }}</p>
+        <h3>{{ product.description }}</h3>
+    </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
